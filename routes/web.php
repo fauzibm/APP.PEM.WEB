91b9d20a8e494433b6532ca4de2907209894e199
miagultom <miagultom9801@gmail.com>
2024-11-16 23:21:12 +0700

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use PhpParser\Builder\Function_;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/halo', Function (){
+//     echo "Halo";
+// });
+
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
